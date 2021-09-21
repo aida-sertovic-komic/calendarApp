e46d53eb543af1ec9a9746ba133d38e360524c00
Aida Sertovic <78958847+aida-sertovic@users.noreply.github.com>
2021-09-21 14:43:06 +0200

    Add files via upload

--- src/app/calendar-component/calendar-component.component.ts
+++ src/app/calendar-component/calendar-component.component.ts
@@ -1,16 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { CalendarService } from '../calendar.service';
 
 @Component({
   selector: 'pm-calendar-component',
   templateUrl: './calendar-component.component.html',
-  styleUrls: ['./calendar-component.component.css']
+  styleUrls: ['./calendar-component.component.css'],
 })
 export class CalendarComponentComponent implements OnInit {
-  value: Date | undefined;
+  value: Date | any;
 
-  // constructor() { }
+  constructor(private calendarService: CalendarService) {}
 
   ngOnInit(): void {
+    this.calendarService.calendarChange.emit(this.value);
   }
 
+  selectedDate(item: Date) {
+    this.calendarService.emitCalendarChangeEvent(item);
+  }
 }
